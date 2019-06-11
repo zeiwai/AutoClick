11229bff19d329841380b469eb41369278b6c668
zeiwai <zylinux94@outlook.com>
2019-06-11 13:06:22 +0800

    implements  keyboard and mouse function

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AutoClick
 AutoClick  send keyboard input (or mouse click) events to the window (or the controls of the window) by mapping some image pattern (or time-based).
+
+acs file syntax:
+comment:  # string
+mouse command: m x,y l/r
+	*x,y is the position,and l/r represent left or right button of the mouse
+keyboard command: k x
+	*x represent the key you want to send to the application. 
+
--- autoclick.c
+++ autoclick.c
@@ -7,6 +7,7 @@
 #include "autoclick.h"
 
 static int virt_dev_fd;
+static int keymap[MAXKEY];
 
 void emit(int fd, int type, int code, int val) {
    struct input_event ie;
@@ -22,10 +23,12 @@ void emit(int fd, int type, int code, int val) {
 
 void ac_keyboard(char *cmdstr)
 {
-   printf("keyboard\n");
-   emit(virt_dev_fd, EV_KEY, KEY_SPACE, 1);
+   char key_c;
+   //printf("*keyboard: %s\n",cmdstr);
+   sscanf(cmdstr,"k %c", &key_c);
+   emit(virt_dev_fd, EV_KEY, keymap[(int)key_c], 1);
    emit(virt_dev_fd, EV_SYN, SYN_REPORT, 0);
-   emit(virt_dev_fd, EV_KEY, KEY_SPACE, 0);
+   emit(virt_dev_fd, EV_KEY, keymap[(int)key_c], 0);
    emit(virt_dev_fd, EV_SYN, SYN_REPORT, 0);
    usleep(15000);
 }
@@ -33,13 +36,28 @@ void ac_keyboard(char *cmdstr)
 void ac_mouse(char *cmdstr)
 {
       int x_i,y_i;
-      char btn_i;
-      printf("%s",cmdstr);
-      sscanf(cmdstr,"m %d,%d %c",&x_i,&y_i,&btn_i);
-      printf("mouse\n");
+      char btn_c;
+      //printf("*mouse: %s\n",cmdstr);
+      sscanf(cmdstr,"m %d,%d %c",&x_i,&y_i,&btn_c);
       emit(virt_dev_fd, EV_REL, REL_X, x_i);
       emit(virt_dev_fd, EV_REL, REL_Y, y_i);
       emit(virt_dev_fd, EV_SYN, SYN_REPORT, 0);
+      switch(btn_c)  {
+	case 'l' :
+	      emit(virt_dev_fd, EV_KEY, BTN_LEFT, 1);
+	      emit(virt_dev_fd, EV_SYN, SYN_REPORT, 0);
+	      emit(virt_dev_fd, EV_KEY, BTN_LEFT, 0);
+	      emit(virt_dev_fd, EV_SYN, SYN_REPORT, 0);
+	      break;
+	case 'r' :
+	      emit(virt_dev_fd, EV_KEY, BTN_RIGHT, 1);
+	      emit(virt_dev_fd, EV_SYN, SYN_REPORT, 0);
+	      emit(virt_dev_fd, EV_KEY, BTN_RIGHT, 0);
+	      emit(virt_dev_fd, EV_SYN, SYN_REPORT, 0);
+	      break;
+	default :
+	      break;
+      }
       usleep(15000);
 }
 
@@ -71,7 +89,6 @@ void ac_exec_acs_file(char *acsfile)
 	}
 	while(getline(&cmdstr,&cmdlen,acs) != -1)
 	{
-		printf("%s",cmdstr);
 		switch(ac_get_cmd_type((char *)cmdstr)) {
 			case AC_KEYBOARD:	ac_keyboard(cmdstr); break;
 			case AC_MOUSE:		ac_mouse(cmdstr); break;
@@ -91,21 +108,24 @@ void init_virt_dev()
 
    virt_dev_fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
 
-   /* enable mouse button left and relative events */
    ioctl(virt_dev_fd, UI_SET_EVBIT, EV_KEY);
-   ioctl(virt_dev_fd, UI_SET_KEYBIT, BTN_LEFT);
 
-   ioctl(virt_dev_fd, UI_SET_EVBIT, EV_KEY);
-   ioctl(virt_dev_fd, UI_SET_KEYBIT, KEY_SPACE);
+   /* enable mouse button events */
+   for (int keycode=0x110; keycode <= 0x117; keycode++)
+	   ioctl(virt_dev_fd, UI_SET_KEYBIT, keycode);
+
+   /* enable keyboard key events */
+   for (int keycode=1; keycode <= 248; keycode++)
+	   ioctl(virt_dev_fd, UI_SET_KEYBIT, keycode);
 
    ioctl(virt_dev_fd, UI_SET_EVBIT, EV_REL);
-   ioctl(virt_dev_fd, UI_SET_RELBIT, REL_X);
-   ioctl(virt_dev_fd, UI_SET_RELBIT, REL_Y);
+   for (int keycode=0x00; keycode <= 0x09; keycode++)
+	   ioctl(virt_dev_fd, UI_SET_RELBIT, keycode);
 
    memset(&usetup, 0, sizeof(usetup));
    usetup.id.bustype = BUS_USB;
-   usetup.id.vendor = 0x1234; /* sample vendor */
-   usetup.id.product = 0x5678; /* sample product */
+   usetup.id.vendor = 0x1234; 
+   usetup.id.product = 0x5678; 
    strcpy(usetup.name, "Example device");
 
    ioctl(virt_dev_fd, UI_DEV_SETUP, &usetup);
@@ -121,7 +141,14 @@ void clean_virt_dev()
 
 int main(void)
 {
+
    init_virt_dev();
+
+   // initial keymap for search.
+   for (int i=0;i<72;i+=2) {
+	   keymap[(int)KEYMAP[i]]=(int)KEYMAP[i+1];
+   }
+
    ac_exec_acs_file("./test.acs");
 
    clean_virt_dev();
